922060ce3952afe9aa90a4f7898252a78797cf03
mstng1967 <haristegui004@gmail.com>
2024-11-12 12:57:13 +0100

    Update update-risk.php

--- app/update-risk.php
+++ app/update-risk.php
@@ -26,22 +26,56 @@ if ($conn->connect_error) {
                                 <h3>Agregar nueva fila</h3>
                                 <table>
                                     <tr>
-                                        <td>User ID</td>
-                                        <td>Financial Year</td>
-                                        <td>Financial Quarter</td>
-                                        <td>Product Name</td>
-                                        <td>Production Unit</td>
-                                        <td>Total Production</td>
-                                        <td>Uploded on</td>
-                                    </tr>
+                    <th>ID del Riesgo</th>
+                    <th>Fecha</th>
+                    <th>Producto</th>
+                    <th>Proceso/Proyecto</th>
+                    <th>Modo de fallo</th>
+                    <th>Efecto de fallo</th>
+                    <th>SEV Inherente</th>
+                    <th>SEV</th>
+                    <th>Causas del modo de fallo</th>
+                    <th>Stakeholders causantes del riesgo</th>
+                    <th>OCC Inherente</th>
+                    <th>OCC</th>
+                    <th>Controles actuales</th>
+                    <th>Control preventivo</th>
+                    <th>Control detectivo</th>
+                    <th>Control correctivo</th>
+                    <th>DET</th>
+                    <th>Bottleneck</th>
+                    <th>RPN Inherente</th>
+                    <th>RPN</th>
+                    <th>Nivel de riesgo</th>
+                    <th>Stakeholders afectados por la materialización del riesgo</th>
+                    <th>¿Aceptable?</th>
+                    <th>Acciones</th>
+                </tr>
                                     <tr>
-                                        <td><input type='text' name='user_id' value=" . $row["user_id"] . "></td>
-                                        <td><input type='text' name='financial_year' value=" . $row["financial_year"] . "></td>
-                                        <td><input type='text' name='financial_quarter' value=" . $row["financial_quarter"] . "></td>
-                                        <td><input type='text' name='product_name' value=" . $row["product_name"] . "></td>
-                                        <td><input type='text' name='production_unit' value=" . $row["production_unit"] . "></td>
-                                        <td><input type='text' name='total_production' value=" . $row["total_production"] . "></td>
-                                        <td><input type='text' name='uploded_on' value=" . $row["uploded_on"] . "></td>
+                                        <td><input type='text' name='risk_id' value=" . $row["risk_id"] . "></td>
+                                        <td><input type='text' name='date' value=" . $row["date"] . "></td>
+                                        <td><input type='text' name='product' value=" . $row["product"] . "></td>
+                                        <td><input type='text' name='process_project' value=" . $row["process_project"] . "></td>
+                                        <td><input type='text' name='failure_mode' value=" . $row["failure_mode"] . "></td>
+                                        <td><input type='text' name='failure_effect' value=" . $row["failure_effect"] . "></td>
+                                        <td><input type='text' name='SEV_inherent' value=" . $row["SEV_inherent"] . "></td>
+                                        <td><input type='text' name='SEV' value=" . $row["SEV"] . "></td>
+                                        <td><input type='text' name='failure_mode_cause' value=" . $row["failure_mode_cause"] . "></td>
+                                        <td><input type='text' name='stakeholders_cause' value=" . $row["stakeholders_cause"] . "></td>
+                                        <td><input type='text' name='OCC_inherent' value=" . $row["OCC_inherent"] . "></td>
+                                        <td><input type='text' name='OCC' value=" . $row["OCC"] . "></td>
+                                        <td><input type='text' name='current_control' value=" . $row["current_control"] . "></td>
+                                        <td><input type='text' name='preventive_control' value=" . $row["preventive_control"] . "></td>
+                                        <td><input type='text' name='detective_control' value=" . $row["detective_control"] . "></td>
+                                        <td><input type='text' name='corrective_control' value=" . $row["corrective_control"] . "></td>
+                                        <td><input type='text' name='DET' value=" . $row["DET"] . "></td>
+                                        <td><input type='text' name='bottleneck' value=" . $row["bottleneck"] . "></td>
+                                        <td><input type='text' name='RPN_inherent' value=" . $row["RPN_inherent"] . "></td>
+                                        <td><input type='text' name='RPN' value=" . $row["RPN"] . "></td>
+                                        <td><input type='text' name='risk_level' value=" . $row["risk_level"] . "></td>
+                                        <td><input type='text' name='stakeholders_affected' value=" . $row["stakeholders_affected"] . "></td>
+                                        <td><input type='text' name='acceptable' value=" . $row["acceptable"] . "></td>
+                                        <td><input type='text' name='actions' value=" . $row["actions"] . "></td>
                                     </tr>
                                     <tr>
                                         <td colspan='10'><button type='submit'>Actualizar</button></td>
@@ -50,17 +84,37 @@ if ($conn->connect_error) {
                             </form>";
             }
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $col1 = $_POST['user_id'];
-                $col2 = $_POST['financial_year'];
-                $col3 = $_POST['financial_quarter'];
-                $col4 = $_POST['product_name'];
-                $col5 = $_POST['production_unit'];
-                $col6 = $_POST['total_production'];
-                $col7 = $_POST['uploded_on'];
+                $col1 = $_POST['risk_id'];
+                $col2 = $_POST['date'];
+                $col3 = $_POST['product'];
+                $col4 = $_POST['process_project'];
+                $col5 = $_POST['failure_mode'];
+                $col6 = $_POST['failure_effect'];
+                $col7 = $_POST['SEV_inherent'];
+                $col8 = $_POST['SEV'];
+                $col9 = $_POST['failure_mode_cause'];
+                $col10 = $_POST['stakeholders_cause'];
+                $col11 = $_POST['OCC_inherent'];
+                $col12 = $_POST['OCC'];
+                $col13 = $_POST['current_control'];
+                $col14 = $_POST['preventive_control'];
+                $col15 = $_POST['detective_control'];
+                $col16 = $_POST['corrective_control'];
+                $col17 = $_POST['DET'];
+                $col18 = $_POST['bottleneck'];
+                $col19 = $_POST['RPN_inherent'];
+                $col20 = $_POST['RPN'];
+                $col21 = $_POST['risk_level'];
+                $col22 = $_POST['stakeholders_affected'];
+                $col23 = $_POST['acceptable'];
+                $col24 = $_POST['actions'];
             
             
-                $sql = "UPDATE mis_production SET user_id =  '$col1', financial_year = '$col2', financial_quarter = '$col3',
-                         product_name = '$col4', production_unit = '$col5', total_production = '$col6', uploded_on = '$col7' WHERE product_name = '$item'";
+                $sql = "UPDATE mis_production SET risk_id =  '$col1', date = '$col2', product = '$col3',
+                         process_project = '$col4', failure_mode = '$col5', failure_effect = '$col6', SEV_inherent = '$col7', SEV = '$col8', failure_mode_cause = '$col9',
+                         stakeholders_cause = '$col10', OCC_inherent = '$col11', OCC = '$col12', current_control = '$col13', preventive_control = '$col14', detective_control = '$col15',
+                         corrective_control = '$col16', DET = '$col17', bottleneck = '$col18', RPN_inherent = '$col19', RPN = '$col20', risk_level = '$col21', 
+                         stakeholders_affected = '$col22', acceptable = '$col23', actions = '$col24' WHERE product_name = '$item'";
             
                 if ($conn->query($sql) === TRUE) {
                     echo "Registro actualizado correctamente";
